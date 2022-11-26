eccf26185af865ca9d52a9043d8960bdb579c4d1
Piyush Chincholikar <piyush@getzippin.com>
2022-11-26 19:21:11 +0530

    Updated cryptographic algorithm using Crypto.Cipher library - updated code including generate IV and encrypt and decrypt using des, 3des, aes - updated executable command w.r.t input file and output path - 3des not working for current code

--- ass1_NS/code/cryptoAlgo.py
+++ ass1_NS/code/cryptoAlgo.py
@@ -1,5 +1,78 @@
-import argparse, os
+import argparse, os, base64
+from Crypto import Random
 from Crypto.Cipher import DES, DES3, AES
+
+# https://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+
+
+def generateIV(block_size):
+    return Random.new().read(block_size)
+
+
+def encryptData(cipher, iv, indata):
+    # Convert string to bytes
+    plaintext = indata.encode("utf-8")
+    # To binary data
+    ciphertext = base64.b64encode(iv + cipher.encrypt(plaintext))
+    return ciphertext
+
+
+def decryptData(cipher, block_size, indata):
+    plaintext = cipher.decrypt(indata[block_size:])
+    # Convert bytes to string
+    result = plaintext.decode("utf-8")
+    return result
+
+
+def getCipher(oper, algo, mode, passphrase, indata):
+    # Convert string to bytes
+    key = passphrase.encode("utf-8")
+    if algo == "des":
+
+        block_size = DES.block_size
+
+        if oper == "enc":
+            iv = generateIV(block_size)
+        if oper == "dec":
+            indata = base64.b64decode(indata)
+            iv = indata[:block_size]
+
+        if mode == "ecb":
+            cipher = DES.new(key, DES.MODE_ECB)
+        elif mode == "cbc":
+            cipher = DES.new(key, DES.MODE_CBC, iv)
+
+    elif algo == "3des":
+
+        block_size = DES3.block_size
+
+        if oper == "enc":
+            iv = generateIV(block_size)
+        if oper == "dec":
+            indata = base64.b64decode(indata)
+            iv = indata[:block_size]
+
+        if mode == "ecb":
+            cipher = DES3.new(key, DES3.MODE_ECB)
+        elif mode == "cbc":
+            cipher = DES3.new(key, DES3.MODE_CBC, iv)
+
+    elif algo == "aes":
+
+        block_size = AES.block_size
+
+        if oper == "enc":
+            iv = generateIV(block_size)
+        if oper == "dec":
+            indata = base64.b64decode(indata)
+            iv = indata[:block_size]
+
+        if mode == "ecb":
+            cipher = AES.new(key, AES.MODE_ECB)
+        elif mode == "cbc":
+            cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    return cipher, iv, block_size, indata
 
 
 def getPassPhrase(keysize):
@@ -34,6 +107,41 @@ def main(oper, algo, mode, keysize, infile, outpath):
     # Get passphrase from user
     passphrase = getPassPhrase(keysize)
 
+    # Get input file data
+    file = open(infile)
+    indata = file.read()
+    file.close()
+
+    # Get cipher for algorithm and mode
+    cipher, iv, block_size, indata = getCipher(oper, algo, mode, passphrase, indata)
+
+    # Encrypt or Decrypt
+    if oper == "enc":
+        outdata = (encryptData(cipher, iv, indata)).decode("utf-8")
+    elif oper == "dec":
+        outdata = decryptData(cipher, block_size, indata)
+
+    input_file = infile[(infile.find("/", infile.find("/") + 1) + 1) :]
+
+    # Creating a file
+    # Put data in output file - can use file name as oper_algo_mode_keysize_infile
+    file = open(
+        outpath
+        + "/"
+        + oper
+        + "_"
+        + algo
+        + "_"
+        + mode
+        + "_"
+        + keysize
+        + "_"
+        + input_file,
+        "w",
+    )
+    file.write(outdata)
+    file.close()
+
 
 # Starting position
 # Cryptographic Encryption-Decryption Analysis
@@ -43,7 +151,7 @@ if __name__ == "__main__":
     1. oper - operations with value 'enc' or 'dec'
     2. algo - algorithm with value 'des', '3des' or 'aes'
     3. mode - mode with value 'ecb' or 'cbc'
-    4. keysize - keysizes in bits for des(64), 3des(168) or aes(128, 192, 256)
+    4. keysize - keysizes in bits for des(64), 3des(192) or aes(128, 192, 256)
     5. infile - input file to be encrypted or decrypted
     6. outpath - output file path
     """
@@ -82,7 +190,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-k",
         "--keysize",
-        help="64/168/128/192/256",
+        help="64/128/192/256",
         type=str,
         action="store",
         required=False,
