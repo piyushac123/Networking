dabcd09cc05e6cf0da3c6355c115196b8f560ed0
Piyush Chincholikar <piyush@getzippin.com>
2023-02-05 12:02:49 +0530

    generated graph for enc_ecb_10K - extracted csv in dataframe and refined it according to enc_ecb_10K - produced bar graph and saved for all combinations of algorithm - updated removal command in script - including example bar graph analysis image

--- SymmetricKeyCryptography/code/bulkResult.py
+++ SymmetricKeyCryptography/code/bulkResult.py
@@ -1,4 +1,6 @@
 import os, csv
+import pandas as pd
+import matplotlib.pyplot as plt
 
 oper = ["enc", "dec"]
 algo = ["des", "3des", "aes"]
@@ -67,13 +69,70 @@ def generateBulkResult():
                             )
                             os.system(cmd)
 
+def refineEncEcb10K(df):
+    df1 = df[df["operation"] == "enc"]
+    df1 = df1[df1["mode"] == "ecb"]
+    df1 = df1[df1["filesize"] == "10K"]
+
+    lst = []
+    lst.append(df1[df1["algorithm"] == "des"]["exec_time"].mean())
+    lst.append(df1[df1["algorithm"] == "3des"]["exec_time"].mean())\
+
+    df1 = df1[df1["algorithm"] == "aes"]
+    lst.append(df1[df1["keysize"] == 128]["exec_time"].mean())
+    lst.append(df1[df1["keysize"] == 192]["exec_time"].mean())
+    lst.append(df1[df1["keysize"] == 256]["exec_time"].mean())
+
+    category = ['DES-64', '3DES-192', 'AES-128', 'AES-192', 'AES-256']
+
+    d = {'category' : category, 'mean': lst}
+    df_enc_ecb_10K = pd.DataFrame(data=d)
+
+    return df_enc_ecb_10K
+
+def generateGraph(df_enc_ecb_10K):
+    fig = plt.figure(figsize = (10, 5))
+    
+    # creating the bar plot
+    plt.bar(df_enc_ecb_10K['category'], df_enc_ecb_10K['mean'], color ='maroon',
+            width = 0.4)
+    
+    plt.xlabel("Category")
+    plt.ylabel("Mean Block encryption time (in secs)")
+    plt.title("Encryption of 10K file using ECB mode")
+
+    if not os.path.exists("testcases_output/plots"):
+        # Create a new directory because it does not exist
+        os.makedirs("testcases_output/plots")
+
+    plt.savefig('testcases_output/plots/enc_ecb_10K.png')
 
 # MAIN FUNCTION
 def main():
     os.system("sh exec.sh -c 22")
 
+    if not os.path.exists("testcases_output/demo"):
+        # Create a new directory because it does not exist
+        os.makedirs("testcases_output/demo")
+
+
     # Generate bulk results
     generateBulkResult()
+
+    # Read csv file data in dataframe
+    df = pd.read_csv('records.csv')
+
+    # Add new column - filesize
+    for ind in range(df.shape[0]):
+        if "10K" in df.loc[ind, "file"]:
+            df.loc[ind, "filesize"] = "10K"
+        elif "1M" in df.loc[ind, "file"]:
+            df.loc[ind, "filesize"] = "1M"
+
+    # Generate graph for different algorithms for - Encryption of 10K file using ECB mode
+    df_enc_ecb_10K = refineEncEcb10K(df)
+    generateGraph(df_enc_ecb_10K)
+
 
     # with open('records.csv', newline='') as csvfile:
     #     reader = csv.DictReader(csvfile)
